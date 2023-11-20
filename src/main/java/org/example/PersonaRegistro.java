ea808ed3bbe995f14ebf6c2b3d6a71397105f3f3
javier <javier.robes@meltoro.com>
2023-11-20 17:29:58 +0100

    Se quitó la linea de codigo inservible

--- src/main/java/org/example/PersonaRegistro.java
+++ src/main/java/org/example/PersonaRegistro.java
@@ -3,8 +3,6 @@ package org.example;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString(){
-        //Esta es la linea de codigo a eliminar
-        //System.out.println("Tests, intruccion insevible");
         return "%s %s".formatted(nombre, apellidos);
     }
 }
